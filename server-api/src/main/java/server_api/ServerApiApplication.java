5c929f4169018f8092d78c2237119b1290cd4abf
Alex Cordaro <zdac051@live.rhul.ac.uk>
2020-12-27 20:42:09 +0000

    Refactored project folder structure.

---
+++ server-api/src/main/java/server_api/ServerApiApplication.java
@@ -0,0 +1,25 @@
+package server_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+public class ServerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApiApplication.class, args);
+	}
+
+	@Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedMethods("GET", "PUT", "POST", "DELETE");
+            }
+        };
+    }
+}
