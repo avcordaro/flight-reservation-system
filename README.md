a5e9c3abfb4f90cf0b87181b1a1294b1fd24a4a1
Alex Cordaro <zdac051@live.rhul.ac.uk>
2020-12-27 16:09:03 +0000

    Setup of frontend and backend.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# flight-reservation-system
+# Flight Reservation System
+
+Web application built using React + Spring Boot + PostgreSQL. 
+
+A flight reservation system for Foobar Airways (fictional), that allows customers to reserve seats for the airline's flights, as well as allowing the airline to manage new/existing flights on offer.
---
+++ frs-springboot-rest-api/src/main/java/frs_springboot_rest_api/FrsSpringbootRestApiApplication.java
@@ -0,0 +1,13 @@
+package frs_springboot_rest_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrsSpringbootRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrsSpringbootRestApiApplication.class, args);
+	}
+
+}
