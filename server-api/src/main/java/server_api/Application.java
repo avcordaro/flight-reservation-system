d55729d604dfb4db2292e5e99868b81a0abc839d
Alex Cordaro <zdac051@live.rhul.ac.uk>
2020-12-27 21:08:49 +0000

    Update .gitignore for server-api

--- server-api/src/main/java/server_api/Application.java
+++ server-api/src/main/java/server_api/Application.java
@@ -7,10 +7,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class ServerApiApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServerApiApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 	@Bean
